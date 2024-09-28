6301d1cc64347c6e189a4ac3c3b7a5aa1e4b8f99
Retzseb <53112791+Retzseb@users.noreply.github.com>
2024-09-28 17:57:28 +0200

    Add files via upload

---
+++ negyedik.py
@@ -0,0 +1,12 @@
+#4.	Készíts egy programot, amely beolvas 3 valós számot, majd kiírja őket, mindegyiket egy sorba szóközzel elválasztva!
+def beolvas():
+    szam = float(input("Adjon meg egy számot!"))
+    return szam
+
+def negyedikFeladat():
+    szam1 = beolvas()
+    szam2 = beolvas()
+    szam3 = beolvas()
+    #print(szam1)
+    # #print(type=(szam1))
+    # print(str(szam1)+" "+str(szam2)+" "+str(szam3))
