5bb47078ec9373ec0fd241748b5d0cc961bf6765
Retzseb <53112791+Retzseb@users.noreply.github.com>
2024-09-28 17:57:11 +0200

    Add files via upload

---
+++ hatos.py
@@ -0,0 +1,15 @@
+#6.	Tárolj el két számot egy-egy változóba! Írd ki az összegüket, különbségüket, szorzatukat és hányadosukat, hatványukat teljes műveleti sorrendben.
+
+import negyedik
+def hatosFeladat():
+
+    szam1 = int(negyedik.beolvas())
+    szam2 = int(negyedik.beolvas())
+
+    print(str(szam1)+" + "+str(szam2)+" = "+str(szam1+szam2))
+    print(str(szam1)+ " - " + str(szam2) + " = " + str(szam1 - szam2))
+    print(str(szam1)+" * "+str(szam2)+" = "+str(szam1*szam2))
+    print(str(szam1)+ " / " + str(szam2) + " = " + str(szam1 / szam2))
+    print(str(szam1)+ " mod " + str(szam2) + " = " + str(szam1 % szam2))
+    print(str(szam1) + " ^ " + str(szam2) + " = " + str(szam1 ** szam2))
+    print(str(szam2) + " ^ " + str(szam1) + " = " + str(szam2 ** szam1))
