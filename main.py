0a87622371b871cf2f9dee672e551165203c591d
Retzseb <53112791+Retzseb@users.noreply.github.com>
2024-09-28 17:57:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#import masodik
+import negyedik
+#import hatos
+#import tizenegy
+import tizenhat
+
+#negyedik.negyedikFeladat()
+#hatos.hatosFeladat()
+#tizenegy.tiezenegyedikFeladat()
+tizenhat.tizenhatodikFeladat()
